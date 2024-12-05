409ac5f7cbebe4edd0f68247e0a66b79456f1637
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-05 21:16:26 +0700

    fix: script umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
         {/* Umami Script */}
-        <Script
+        <script
+          async
           defer
           src='https://cloud.umami.is/script.js'
           data-website-id='a4e1f312-450a-43c1-9a1a-b0df40fb9248'
