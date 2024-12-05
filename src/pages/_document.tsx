bf66ed1f71c253db9f76e91ec5215b38c6c9629b
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-05 21:02:06 +0700

    chore: setup umami script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        {/* Umami Script */}
+        <Script
+          defer
+          src='https://cloud.umami.is/script.js'
+          data-website-id='a4e1f312-450a-43c1-9a1a-b0df40fb9248'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
