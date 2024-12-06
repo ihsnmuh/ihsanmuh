0cb0ce470c0a33a82e24a8812c2388648e56256e
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-06 07:59:58 +0700

    chore: add local umami script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,14 @@ export default function Document() {
           src='https://cloud.umami.is/script.js'
           data-website-id='a4e1f312-450a-43c1-9a1a-b0df40fb9248'
         />
+
+        {/* Umami Script Local Domain */}
+        <script
+          async
+          defer
+          src='https://analytic.ihsanmuh.my.id/script.js'
+          data-website-id='59b2fd70-9256-4b86-819a-ba97a021c10d'
+        />
       </Head>
       <body>
         <Main />
