e51c0406f36f0d9d3a3f28b96418c95b88fe8b1c
Muhammad Ihsan <muhammadihsan076@gmail.com>
2026-02-21 10:26:36 +0700

    feat: integrate Microsoft Clarity for enhanced user analytics
    - Added Microsoft Clarity tracking script to the document for improved user behavior insights.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,19 @@ export default function Document() {
           src={`${process.env.NEXT_PUBLIC_UMAMI_DOMAIN}/script.js`}
           data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
         />
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vkncrl4j1n");
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
